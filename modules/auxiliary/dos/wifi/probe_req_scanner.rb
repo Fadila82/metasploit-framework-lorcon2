e0e3f11d2b8b06cd13db1d84a22a663a3f50d463
Fadila <fadila.khadar@gmail.com>
2018-11-19 17:22:19 +0100

    WIP: ieee80211 parser + probe requests scanner example

---
+++ modules/auxiliary/dos/wifi/probe_req_scanner.rb
@@ -0,0 +1,84 @@
+# coding: utf-8
+
+
+##
+# This module requires Metasploit: http//metasploit.com/download
+
+# Current source: https://github.com/rapid7/metasploit-framework
+##
+
+require 'msf/core'
+require_relative 'ieee80211'
+
+
+
+
+
+class DeviceInfos
+  def initialize(macAddress = "")
+    if (macAddress == nil)
+      print "Error has to be handled here\n"
+    end
+    # should check addr format
+    @macAddress = macAddress
+  end
+  attr_accessor :macAddress
+end
+
+
+class MetasploitModule < Msf::Auxiliary
+
+    
+  include Msf::Exploit::Lorcon2
+  include Msf::Auxiliary::Dos
+
+  def initialize(info ={})
+    super(update_info(info,
+      'Name'		=> 'Wireless Listener',
+      'Description' 	=> %q{
+          Print sender mac addresses of received probe requests and probe requests count 
+      },
+       'Author'	=> [ 'Fadila Khadar' ],
+      'License'	=> MSF_LICENSE
+    ))
+
+    register_options(
+      [
+      ],self.class)
+  end
+
+  
+  def run
+
+    print_status("Open wireless interface")
+
+    open_wifi
+    devices = Hash.new(0)
+    
+    # pcap filter to only capture probe requests
+    set_filter("type mgt subtype probe-req")
+    clear_screen
+    print_tab_header
+    each_packet { | pkt | process_packet(pkt,devices) }
+  end
+
+  def process_packet(pkt,devices)
+    # Parse pkt as 802.11 management frame
+    managementFrame = ManagementFrame.new("#{pkt.rawdata}")
+    #update packet count for this device
+    devices[managementFrame.frame80211.addr2] = devices[managementFrame.frame80211.addr2] + 1
+    clear_screen
+    print_tab_header
+    devices.each{|mac,count| print " #{mac} \t\t #{count}\n"}
+  end
+
+  def clear_screen
+    printf("\x1B[2J")
+    printf("\n")
+  end
+  def print_tab_header
+    print "\tMAC\t\t\tcount\n"
+    print " ----------------------------------------\n"
+
+  end
+end
---
+++ modules/auxiliary/dos/wifi/radiotap.rb
@@ -0,0 +1,20 @@
+class RadioTap
+  def initialize(content_as_string = "")
+    @content_as_string = content_as_string
+    # radiotap header format:
+    # - header revision: 2
+    # - header pad: 2
+    # - header length: 4
+    tab = @content_as_string.unpack('CCSa*')
+    @header_revision = tab[0]
+    @header_pad = tab[1]
+    @header_length = tab[2]
+    # compute payload from the retrieved header length
+    tab = @content_as_string.unpack('x' + @header_length.to_s + 'a*')
+    print tab.length
+    print "\n"
+    @payload = tab[0]
+  end
+  attr_accessor :header_length
+  attr_accessor :payload
+end
